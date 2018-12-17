8f7e391ed1e527374282df3611e8197375007d6b
Habemus <hello@habem.us>
2018-12-17 12:26:19 -0200

    Initial page Home, carbon fields structure

--- src/inc/carbon-fields/page-template-home.php
+++ src/inc/carbon-fields/page-template-home.php
@@ -2,6 +2,13 @@
 use Carbon_Fields\Container;
 use Carbon_Fields\Field;
 
+Container::make('post_meta', 'Abertura')
+  ->where('post_template', '=', 'page-templates/home.php')
+  ->add_fields(array(
+    Field::make('text', 'ca_home__abertura_title', 'Título'),
+    Field::make('rich_text', 'ca_home__abertura_description', 'Descrição'),
+  ));
+
 Container::make('post_meta', 'Eixos de atuação')
   ->where('post_template', '=', 'page-templates/home.php')
   ->add_fields(array(
@@ -12,15 +19,50 @@ Container::make('post_meta', 'Eixos de atuação')
       ->add_fields(array(
         Field::make('text', 'title', 'Título'),
         Field::make('rich_text', 'description', 'Descrição')
+        ->set_default_value(array(
+          array(
+            'title' => '',
+            'description' => ''                    
+          )
+        )),
       ))
       ->set_header_template('<%- $_index + 1 %> <% if (title) { %>- <%- title %><% } %>'),
   ));
+Container::make('post_meta', 'Sobre')
+  ->where('post_template', '=', 'page-templates/home.php')
+  ->add_fields(array(
+    Field::make('text', 'ca_home__sobre_title', 'Título'),
+    Field::make('rich_text', 'ca_home__sobre_description', 'Descrição'),
+    Field::make('text', 'ca_home__sobre_title_quem_faz', 'Título de quem faz'),
+    Field::make('complex', 'ca_home__sobre_quem_faz', 'Quem faz')
+      ->add_fields(array(
+        Field::make('image', 'image', 'Imagem'),
+      )),
+    Field::make('text', 'ca_home__sobre_title_quem_apoia', 'Título de quem apoia'),
+    Field::make('complex', 'ca_home__sobre_quem_apoia', 'Quem apoia')
+      ->add_fields(array(
+        Field::make('image', 'image', 'Imagem'),
+      ))
+  ));
 
-// Container::make('post_meta', 'Projetos')
-//   ->where('post_template', '=', 'page-templates/home.php')
+Container::make('post_meta', 'Lista de Projetos')
+  ->where('post_template', '=', 'page-templates/home.php')
+  ->add_fields(array(
+    Field::make('text', 'ca_home__project_title', 'Título'),
+    Field::make('complex', 'ca_home__project', 'Projetos')
+      ->set_layout('tabbed-horizontal')
+      ->add_fields(array(
+        Field::make('text', 'title', 'Título'),
+        Field::make('textarea', 'description', 'Descrição'),
+        Field::make('text', 'button', 'Botão'),
+        Field::make('image', 'image', 'Imagem'),
+      ))
+    ->set_header_template('<%- $_index + 1 %> <% if (title) { %>- <%- title %><% } %>'),
+  ));
 
 Container::make('post_meta', 'Contato')
   ->where('post_template', '=', 'page-templates/home.php')
   ->add_fields(array(
+    Field::make('text', 'ca_home__contact_title', 'Título'),
     Field::make('text', 'ca_home__contact_form_shortcode', 'Shortcode do formulário de contato')
   ));
--- src/page-templates/home.php
+++ src/page-templates/home.php
@@ -8,30 +8,71 @@ get_header(); ?>
 
 <main id="page-template-home">
 	<section
-		id="#sobre"
+		id="#abertura"
 		class="ca-bg-red">
 		<div class="max-width-container side-padding-container container">
 
 			<div class="row">
 				<div class="ca-page-header col-md-9 offset-md-3">
 					<h1 class="ca-heading-1">
-						Como anda é o ponto de encontro de organizações que promovem mobilidade a pé no Brasil
+					<?php echo carbon_get_post_meta(get_the_ID(), 'ca_home__abertura_title');  ?>
 					</h1>
 					<div class="wysiwyg-content">
-						<?php the_content(); ?>
+						<?php echo apply_filters('the_content',carbon_get_post_meta(get_the_ID(), 'ca_home__abertura_description')); ?>
 					</div>
 				</div>
 			</div>
-
-
-			<ul></ul>
+      <?php $ca_home__eixos = carbon_get_post_meta(get_the_ID(), 'ca_home__eixos'); ?>
+			<ul>
+        <?php foreach($ca_home__eixos as $eixo) : ?>
+        <li>
+          <?php echo $eixo['title']; ?>
+          <?php echo $eixo['description']; ?>
+        </li>
+        <?php endforeach; ?>          
+      </ul>
 		</div>
 	</section>
+  <section
+    id="#projetos">
+    <div class="max-width-container side-padding-container ">
+      <h1 class="ca-heading-1">
+      <?php echo carbon_get_post_meta(get_the_ID(), 'ca_home__project_title'); ?>
+      </h1>
+      <?php $ca_home__project = carbon_get_post_meta(get_the_ID(), 'ca_home__project'); ?>
+      <ul>
+        <?php foreach($ca_home__project as $item) : ?>
+        <li>
+          <h2 class="ca-heading-2"><?php echo $item['title']; ?></h2>
+          <p><?php echo $item['description']; ?></p>
+          <button><?php echo $item['button']?></button>
+          <img src="<?php echo wp_get_attachment_image_src($item['image'], 'thumbnail')[0]; ?>">
+        </li>
+        <?php endforeach; ?>          
+      </ul>
+    </div>
+  </section>
+  <section
+    id="#sobre">
+    <div class="max-width-container side-padding-container">
+      <h1 class="ca-heading-1">
+      <?php echo carbon_get_post_meta(get_the_ID(), 'ca_home__sobre_title');  ?>
+      </h1>
+      <p><?php echo carbon_get_post_meta(get_the_ID(), 'ca_home__sobre_description'); ?></p>
+      <h3><?php echo carbon_get_post_meta(get_the_ID(), 'ca_home__sobre_quem_faz_title');?></h3>
+      <?php $ca_home__sobre_quem_faz = carbon_get_post_meta(get_the_ID(), 'ca_home__sobre_quem_faz'); ?>
+      <?php foreach($ca_home__sobre_quem_faz as $item) : ?>
+        <img src="<?php echo wp_get_attachment_image_src($item['image'], 'thumbnail')[0]; ?>">
+      <?php endforeach; ?>  
+    </div>
+  </section>
 	<section
 		id="contato"
 		class="ca-bg-blue">
 		<div class="max-width-container side-padding-container">
-
+      <h1 class="ca-heading-1">
+      <?php echo carbon_get_post_meta(get_the_ID(), 'ca_home__contact_title');  ?>
+      </h1>
 			<div class="ca-form">
 				<?php echo do_shortcode(carbon_get_post_meta(get_the_ID(), 'ca_home__contact_form_shortcode')); ?>
 			</div>
