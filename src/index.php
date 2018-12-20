02f5b7e522b24d34f7143c496044d679bcc5ca79
Simon <simonxd@gmail.com>
2018-12-20 17:30:50 -0200

    Implements mural skeleton

--- src/index.php
+++ src/index.php
@@ -14,9 +14,106 @@
 
 get_header(); ?>
 
-<?php while (have_posts()) : the_post(); ?>
-	<?php the_title(); ?>
-<?php endwhile; ?>
+<main id="page-template-mural" class="ca-bg-red-light">
+
+	<section id="mural-abertura">
+		<div class="container max-width-container side-padding-container ca-padding-top-6 ca-section-header">
+			<h1 class="ca-heading-1 ca-section-header__heading ca-padding-bottom-3">Mural Como Anda</h1>
+		</div>
+	</section>
+
+	<section id="mural-postagens">
+		<div class="container max-width-container side-padding-container ca-padding-top-6 ca-section-header">
+			<div class="row">
+				<div class="col-md-8">
+					<div>
+						<?php while (have_posts()) : the_post(); ?>
+						<article class="ca-mural-post">
+							<div class="ca-mural-post__left">
+								<h4 class="ca-mural-post__left__date"><?php the_date(); ?></h4>
+								
+								<a href="<?php the_permalink(); ?>">
+									<h2 class="ca-mural-post__left__heading"><?php the_title(); ?></h2>
+								</a>
+								<a
+									class="ca-mural-post__left__excerpt"
+									href="<?php the_permalink(); ?>">
+									<?php the_excerpt(); ?>
+								</a>
+							</div>
+							<div class="ca-mural-post__right">
+								<a
+									class="ca-mural-post__right__thumbnail"
+									href="<?php the_permalink(); ?>">
+									<?php the_post_thumbnail(); ?>
+								</a>
+								<div class="ca-tag-list">
+									<?php the_tags('', ''); ?>
+								</div>
+							</div>
+						</article>
+						<?php endwhile; ?>
+					</div>
+				</div>
+
+				<aside class="col-md-4">
+				  <div>
+				    <?php get_search_form(true); ?>
+				  </div>
+
+					<!-- sticky posts -->
+				  <?php
+
+				  $ca_mural__sticky_posts_query = new WP_Query(array(
+				    'posts_per_page' => 10,
+				    'post__in'  => get_option('sticky_posts'),
+				    'ignore_sticky_posts' => 1
+				  ));
+				  ?>
+
+				  <?php if ($ca_mural__sticky_posts_query->have_posts()) : ?>
+					<div>
+						<h4>Destaques</h4>
+						<div>
+							<?php while ($ca_mural__sticky_posts_query->have_posts()) : $ca_mural__sticky_posts_query->the_post(); ?>
+
+			        <article class="swiper-slide">
+			          <a href="<?php the_permalink(); ?>">
+			            <?php the_post_thumbnail(); ?>
+			            <h4>
+			              <?php the_title(); ?>
+			            </h4>
+			            <h5>
+			              <?php echo get_the_date(); ?>
+			            </h5>
+			          </a>
+			        </article>
+
+			        <?php endwhile; wp_reset_postdata(); ?>
+						</div>
+					</div>
+					<?php endif; ?>
+
+					<?php $ca_mural__all_tags = get_tags(); ?>
+					<?php if (count($ca_mural__all_tags) > 0) : ?>
+					<div>
+						<?php foreach ($ca_mural__all_tags as $tag) : ?>
+						<?php $tag_href = get_tag_link($tag->term_id); ?>
+						<a
+							href="<?php echo $tag_href; ?>"
+							title="<?php echo $tag->name; ?>">
+							<?php echo $tag->name; ?>
+						</a>
+						<?php endforeach; ?>
+					</div>
+					<?php endif; ?>
+					
+				</aside>
+			</div>
+		</div>
+	</section>
+
+</main>
 
 <?php
 get_footer();
