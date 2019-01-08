f690e1d6737ad0c092e2c03717f97d9b5c249ffc
Simon <simonxd@gmail.com>
2019-01-08 13:24:48 -0200

    Adds image size specific for block pages

--- inc/image-sizes.php
+++ inc/image-sizes.php
@@ -9,3 +9,8 @@ add_image_size('ca-link-image', 400, 800, false);
  * Used in timelines (width is most important)
  */
 add_image_size('ca-timeline-image', 600, 800, false);
+
+/**
+ * Used in the page template blocks
+ */
+add_image_size('ca-square-image', 500, 500, false);
